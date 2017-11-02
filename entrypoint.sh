96a2a928625c422f8081f70454046018e88ac6c8
Max Metral <max@pyralis.com>
2017-11-02 16:32:59 -0400

    Fix npm location

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ if [ "$1" = 'node-app' ]; then
   [ -f /config/production.json ] && ln -sf /config/production.json config/production.json
   [ "$#" -gt 0 -a "$1" != '--' ] && set -- '--' "$@"
   if [ "$#" -gt 0 ]; then
-    exec tini -- su-exec node-app /usr/bin/npm start "$@"
+    exec tini -- su-exec node-app /usr/local/bin/npm start "$@"
   else
-    exec tini -- su-exec node-app /usr/bin/npm start
+    exec tini -- su-exec node-app /usr/local/bin/npm start
   fi
 fi
 
